dfba8ec8eb2847c02cd9d3b0fe5893399a148c8e
mint-made <thomaskupai@gmail.com>
2021-02-14 16:26:18 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Accordion from "./components/Accordion";
+import Search from "./components/Search";
 
-function App() {
+const items = [
+  {
+    title: "What is React?",
+    content: "React is a front-end js framework",
+  },
+  {
+    title: "Why use React?",
+    content: "React is a favourite js libary under engineers",
+  },
+  {
+    title: "Why do you use React?",
+    content: "You use React by creating components",
+  },
+];
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Search />
     </div>
   );
-}
+};
 
 export default App;
